1a3959cb4d807c17242e791e6415d3dd3c369f85
gustavo.umbelino <gustavo.umbelino@gpnet.com.br>
2025-06-29 17:03:30 -0300

    ajuste de estilo

--- src/app/components/layout/NavBar.tsx
+++ src/app/components/layout/NavBar.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <div className="w-full flex bg-zinc-900 absolute p-0">
-      <Image src="/logo1.png" className="p-2" alt="" width={90} height={90}></Image>
+      <Link href="/"><Image src="/logo1.png" className="p-2" alt="" width={90} height={90}></Image></Link>
     </div>
   );
 }
